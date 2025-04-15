fc7da3004db689300558ec552d9c693a4e5385f1
Brunosz2712 <brunosouza_27@yahoo.com.br>
2025-04-15 10:33:15 -0300

    first commit

---
+++ src/main/java/br/com/fiap/Projeto_Mercado_Presentation/ProjetoMercadoPresentationApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Projeto_Mercado_Presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMercadoPresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMercadoPresentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/Projeto_Mercado_Presentation/ProjetoMercadoPresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Projeto_Mercado_Presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMercadoPresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
